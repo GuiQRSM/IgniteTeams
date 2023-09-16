6a623b8f357cd925dcf0b078afe8a38a2efdd2c6
GuiQRSM <endbeob15@gmail.com>
2023-09-15 22:30:03 -0300

    Makes a small correction to the 'babel-plugin-module-resolver' configuration.

--- babel.config.js
+++ babel.config.js
@@ -3,19 +3,21 @@ module.exports = function (api) {
   return {
     presets: ["babel-preset-expo"],
     plugins: [
-      "module-resolver",
-      {
-        root: ["./src"],
-        alias: {
-          "@assets": "./src/assets", //'alias(apelido)': './endereço original/***/,'
-          "@components": "./src/components",
-          "@routes": "./src/routes",
-          "@screens": "./src/screens",
-          "@storage": "./src/storage",
-          "@theme": "./src/theme",
-          "@utils": "./src/utils", //funções que reaproveitaremos
+      [
+        "module-resolver",
+        {
+          root: ["./src"],
+          alias: {
+            "@assets": "./src/assets", //'alias(apelido)': './endereço original/***/,'
+            "@components": "./src/components",
+            "@routes": "./src/routes",
+            "@screens": "./src/screens",
+            "@storage": "./src/storage",
+            "@theme": "./src/theme",
+            "@utils": "./src/utils", //funções que reaproveitaremos
+          },
         },
-      },
+      ],
     ],
   };
 };
