fa85102b7d389477977964563687d9572d8bdf45
GuiQRSM <endbeob15@gmail.com>
2023-09-17 12:24:07 -0300

    Refactors styling properties using web CSS rules.

--- src/screens/Groups/styles.ts
+++ src/screens/Groups/styles.ts
@@ -1,8 +1,8 @@
 import styled from "styled-components/native";
 
 export const Container = styled.View`
-flex: 1,
-backgroundColor: "#000",
-alignItems: "center",
-justifyContent: "center",
+  flex: 1;
+  background-color: #000;
+  align-items: center;
+  justify-content: center;
 `;
