9ba9dd8681219aedd22705f0a6aeea3603a39853
GuiQRSM <endbeob15@gmail.com>
2023-09-19 15:48:00 -0300

    Apply small corrections.

--- src/components/Loading/styles.ts
+++ src/components/Loading/styles.ts
@@ -4,9 +4,9 @@ export const Container = styled.View`
   flex: 1;
   justify-content: center;
   align-items: center;
-  background-color: ${(theme) => theme.COLORS.GRAY_600};
+  background-color: ${({ theme }) => theme.COLORS.GRAY_600};
 `;
 
-export const LoadIndicator = styled.ActivityIndicator.attr(({ theme }) => ({
+export const LoadIndicator = styled.ActivityIndicator.attrs(({ theme }) => ({
   color: theme.COLORS.GREEN_700,
 }))``;
