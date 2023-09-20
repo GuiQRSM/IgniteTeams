5f4b52a333bc1f08d955823f3ab2c0f1b13a6772
GuiQRSM <endbeob15@gmail.com>
2023-09-20 17:35:44 -0300

    Sets a 'TouchableOpacity' in 'index.tsx' of the 'Header'.

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,10 +1,12 @@
-import { Container, Logo, BackIcon } from "./styles";
+import { Container, Logo, BackIcon, BackButton } from "./styles";
 import logoImg from "@assets/logo.png";
 
 export function Header() {
   return (
     <Container>
-      <BackIcon />
+      <BackButton>
+        <BackIcon />
+      </BackButton>
       <Logo source={logoImg} />
     </Container>
   );
