34a2bacc493e06360e056251e141b022d90ede88
GuiQRSM <endbeob15@gmail.com>
2023-09-20 17:08:23 -0300

    Removes and sets icon import in 'styles.ts' of 'Header'.

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,10 +1,10 @@
 import { Container, Logo } from "./styles";
 import logoImg from "@assets/logo.png";
-import { CaretLeft } from "phosphor-react-native";
 
 export function Header() {
   return (
     <Container>
+      <CaretLeft color="#fff" />
       <Logo source={logoImg} />
     </Container>
   );
