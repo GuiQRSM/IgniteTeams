55c7b18f5cb36e1f631e90450648f08d60a10895
GuiQRSM <endbeob15@gmail.com>
2023-09-21 23:27:30 -0300

    Add the hifglight styling settings to the 'Highlight' folder within 'style.ts'.

---
+++ src/components/Highlight/style.ts
@@ -0,0 +1,20 @@
+import styled from "styled-components/native";
+
+expo const Container = styled.View`
+width: %;
+margin: 32px 0;
+`;
+
+export const Title = styled.Text`
+text-align: center;
+font-size: ${{(theme)} => theme.FONT.SIZE.XL}px;
+font-family: ${{(theme)} => theme.FONT.FAMILY.BOLD};
+color: ${{(theme)} => theme.FONT.COLORS.WHITE}
+`
+
+export const Subtitle = styled.Text`
+text-align: center;
+font-size: ${{(theme)} => theme.FONT.SIZE.MD}px;
+font-family: ${{(theme)} => theme.FONT.FAMILY.REGULAR};
+color: ${{(theme)} => theme.FONT.COLORS.GRAY_300}
+`
