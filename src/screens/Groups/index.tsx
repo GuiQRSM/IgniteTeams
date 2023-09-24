4531432031f87c672bae092b9129f3d4095b16ee
GuiQRSM <endbeob15@gmail.com>
2023-09-23 23:21:58 -0300

    Defines the import of the 'GroupCard' component in the 'indextsx' of 'Groups'.

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,12 +1,15 @@
 import * as S from "./styles";
 import { Header } from "@components/Header";
 import { HighLight } from "@components/Highlight";
+import { GroupCard } from "@components/GroupCard";
 
 export function Groups() {
   return (
     <S.Container>
       <Header showBackButton />
       <HighLight title="Turmas" subtitle="Jogue com a sua turma!" />
+      import {GroupCard} from "@components/GroupCard";
+      <GroupCard title="Calma Cara!" />;
     </S.Container>
   );
 }
