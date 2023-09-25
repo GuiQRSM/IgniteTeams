457ae53f2d5602483eed525f6436270ece4bca95
GuiQRSM <endbeob15@gmail.com>
2023-09-25 15:22:22 -0300

    Prepares the ground for bug fixes.

--- src/components/GroupCard/styles.ts
+++ src/components/GroupCard/styles.ts
@@ -3,14 +3,14 @@ import { TouchableOpacity } from "react-native";
 import { UsersThree } from "phosphor-react-native";
 
 export const Container = styled(TouchableOpacity)`
-width: 100%;
-height: 90px;
-background-color: ${({ theme }) => theme.COLORS.GRAY_600}
-border-radius: 6px;
-flex-direction: row;
-align-items: center;
-padding: 24px;
-margin-bottom: 12px;
+  width: 100%;
+  height: 90px;
+  background-color: ${({ theme }) => theme.COLORS.GRAY_500};
+  border-radius: 6px;
+  flex-direction: row;
+  align-items: center;
+  padding: 24px;
+  margin-bottom: 12px;
 `;
 
 export const Title = styled.Text`
